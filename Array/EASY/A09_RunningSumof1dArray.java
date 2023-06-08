c188b967f9bbe4a386c226dcf39ced2f5c533931
itssarbajjet <sarbajeetmohanty110@gmail.com>
2023-06-08 20:53:01 +0530

    initial commit

---
+++ Array/EASY/A09_RunningSumof1dArray.java
@@ -0,0 +1,18 @@
+public class A09_RunningSumof1dArray {
+    public static void main(String[] args) {
+        int[] arr = {1, 2, 3, 4};
+        int[] runningSum = runningSum(arr);
+        
+        // Printing the running sum
+        for (int num : runningSum) {
+            System.out.print(num + " ");
+        }
+    }
+    
+    public static int[] runningSum(int[] nums) {
+        for (int i = 1; i < nums.length; i++) {
+            nums[i] = nums[i] + nums[i - 1];
+        }
+        return nums;
+    }
+}
