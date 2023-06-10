99a8bde0df6a69109b9c2ebc68ae3b470b9e5660
itssarbajjet <sarbajeetmohanty110@gmail.com>
2023-06-10 19:51:09 +0530

    initial commit

---
+++ Array/EASY/A14_ShuffleString.java
@@ -0,0 +1,22 @@
+public class A14_ShuffleString {
+    public String restoreString(String s, int[] indices) {
+        char[] restored = new char[s.length()];
+        
+        for (int i = 0; i < s.length(); i++) {
+            restored[indices[i]] = s.charAt(i);
+        }
+        
+        return new String(restored);
+    }
+    
+    public static void main(String[] args) {
+        A14_ShuffleString solution = new A14_ShuffleString();
+        
+        // Example usage
+        String s = "codeleet";
+        int[] indices = {4,5,6,7,0,2,1,3};
+        
+        String restoredString = solution.restoreString(s, indices);
+        System.out.println(restoredString);
+    }
+}
