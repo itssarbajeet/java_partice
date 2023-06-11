6a5d90d225dc43e19d05e4eaa481280790d67b39
itssarbajjet <sarbajeetmohanty110@gmail.com>
2023-06-11 17:31:15 +0530

    initial

--- Array/EASY/A09_RunningSumof1dArray.java
+++ Array/EASY/A09_RunningSumof1dArray.java
@@ -7,8 +7,7 @@ public class A09_RunningSumof1dArray {
         for (int num : runningSum) {
             System.out.print(num + " ");
         }
-    }
-    
+    } 
     public static int[] runningSum(int[] nums) {
         for (int i = 1; i < nums.length; i++) {
             nums[i] = nums[i] + nums[i - 1];
