c40e8897850b9c70da2a8fb92602da699e569b5b
itssarbajjet <sarbajeetmohanty110@gmail.com>
2023-06-12 21:29:09 +0530

    intial

--- Array/EASY/A21_TruncateSentence.java
+++ Array/EASY/A21_TruncateSentence.java
@@ -22,3 +22,23 @@ public class A21_TruncateSentence {
         System.out.println(truncatedSentence);
     }
 }
+/*
+ * Input: s = "Hello how are you Contestant", k = 4
+Output: "Hello how are you"
+Explanation:
+The words in s are ["Hello", "how" "are", "you", "Contestant"].
+The first 4 words are ["Hello", "how", "are", "you"].
+Hence, you should return "Hello how are you".
+Example 2:
+
+Input: s = "What is the solution to this problem", k = 4
+Output: "What is the solution"
+Explanation:
+The words in s are ["What", "is" "the", "solution", "to", "this", "problem"].
+The first 4 words are ["What", "is", "the", "solution"].
+Hence, you should return "What is the solution".
+Example 3:
+
+Input: s = "chopper is not a tanuki", k = 5
+Output: "chopper is not a tanuki"
+ */
