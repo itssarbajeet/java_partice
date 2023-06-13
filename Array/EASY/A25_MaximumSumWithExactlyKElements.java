8a558ed280f1d9cc39d57995f39c35f2583d06e4
itssarbajjet <sarbajeetmohanty110@gmail.com>
2023-06-13 19:38:24 +0530

    initial

--- Array/EASY/A25_MaximumSumWithExactlyKElements.java
+++ Array/EASY/A25_MaximumSumWithExactlyKElements.java
@@ -2,8 +2,6 @@ class A25_MaximumSumWithExactlyKElements{
 
     public static void main(String[] args) {
         A25_MaximumSumWithExactlyKElements solution = new A25_MaximumSumWithExactlyKElements();
-
-        // Example usage
         int[] nums = {1, 2, 3, 4, 5};
         int k = 3;
         int result = solution.maximizeSum(nums, k);
@@ -13,8 +11,6 @@ class A25_MaximumSumWithExactlyKElements{
     public int maximizeSum(int[] nums, int k) {
         int max = nums[0];
         int sum = 0;
-
-        // Find the maximum number in the array
         for (int i = 1; i < nums.length; i++) {
             if (max < nums[i]) {
                 max = nums[i];
@@ -22,7 +18,6 @@ class A25_MaximumSumWithExactlyKElements{
         }
 
         int count = 0;
-        // Add the maximum number to the sum for 'k' times
         for (int i = 0; i < k; i++) {
             sum += max + count;
             count++;
