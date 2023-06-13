8c08ea2a2ccaaf5434aa2336c2ff9b91ed378d25
itssarbajjet <sarbajeetmohanty110@gmail.com>
2023-06-13 17:26:17 +0530

    intial

--- Array/EASY/A24_CountNumberofPairsWithAbsoluteDifferenceK.java
+++ Array/EASY/A24_CountNumberofPairsWithAbsoluteDifferenceK.java
@@ -20,3 +20,17 @@ public class A24_CountNumberofPairsWithAbsoluteDifferenceK {
         System.out.println("Number of pairs with absolute difference " + k + ": " + result);
     }
 }
+/*
+ * Input: nums = [1,2,2,1], k = 1
+Output: 4
+Explanation: The pairs with an absolute difference of 1 are:
+- [1,2,2,1]
+- [1,2,2,1]
+- [1,2,2,1]
+- [1,2,2,1]
+Example 2:
+
+Input: nums = [1,3], k = 3
+Output: 0
+Explanation: There are no pairs with an absolute difference of 3.
+ */
