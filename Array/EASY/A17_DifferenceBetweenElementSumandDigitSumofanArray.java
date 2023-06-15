60c8cdaae19cecfa851e77fbdedfdf7a0ac81e48
itssarbajjet <sarbajeetmohanty110@gmail.com>
2023-06-15 23:02:41 +0530

    iniTIAL

--- Array/EASY/A17_DifferenceBetweenElementSumandDigitSumofanArray.java
+++ Array/EASY/A17_DifferenceBetweenElementSumandDigitSumofanArray.java
@@ -27,4 +27,4 @@ Explanation:
 The element sum of nums is 1 + 15 + 6 + 3 = 25.
 The digit sum of nums is 1 + 1 + 5 + 6 + 3 = 16.
 The absolute difference between the element sum and digit sum is |25 - 16| = 9.
- */
+ */ 
