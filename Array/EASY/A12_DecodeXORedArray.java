69ccd42659e5a4ff144d444ced4e1edb38985791
itssarbajjet <sarbajeetmohanty110@gmail.com>
2023-06-15 23:01:19 +0530

    iniTIAL

--- Array/EASY/A12_DecodeXORedArray.java
+++ Array/EASY/A12_DecodeXORedArray.java
@@ -29,6 +29,6 @@ public class A12_DecodeXORedArray {
 /*
  * Input: encoded = [1,2,3], first = 1
 Output: [1,0,2,1]
-Explanation: If arr = [1,0,2,1], then first = 1 and encoded = [1 XOR 0, 0 XOR 2, 2 XOR 1] = [1,2,3]
+Explanation: If arr  = [1,0,2,1], then first = 1 and encoded = [1 XOR 0, 0 XOR 2, 2 XOR 1] = [1,2,3]
  */
 
