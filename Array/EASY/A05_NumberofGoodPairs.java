ec3c8e5c7865af234e1a1e3ab07a2520a798fe10
itssarbajjet <sarbajeetmohanty110@gmail.com>
2023-06-15 23:01:35 +0530

    iniTIAL

--- Array/EASY/A05_NumberofGoodPairs.java
+++ Array/EASY/A05_NumberofGoodPairs.java
@@ -20,7 +20,7 @@ public class A05_NumberofGoodPairs {
 }
 /*
  * Input: nums = [1,2,3,1,1,3]
-Output: 4
+Output: 4 
 Explanation: There are 4 good pairs (0,3), (0,4), (3,4), (2,5) 0-indexed.
 Example 2:
 
