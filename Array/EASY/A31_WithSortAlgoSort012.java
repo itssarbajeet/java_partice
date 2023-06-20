c122d2db28599c12e1dbf9d1cdf07877ddb56f85
itssarbajjet <sarbajeetmohanty110@gmail.com>
2023-06-20 23:54:57 +0530

    initial

--- Array/EASY/A31_WithSortAlgoSort012.java
+++ Array/EASY/A31_WithSortAlgoSort012.java
@@ -15,6 +15,46 @@ public class A31_WithSortAlgoSort012 {
     
 }
 /*
+ * Optiimised code (Dutch National FLag Algorithm)
+class Solution {
+
+    public void swap(int arr[],int x,int y)
+    {
+        int temp = arr[x];
+        arr[x] = arr[y];
+        arr[y] = temp;
+    }
+
+    public void sortColors(int[] arr)
+    {
+
+        int low = 0;
+        int mid = 0;
+        int high = arr.length-1;
+
+        for(int i = 0; i < arr.length; i++)
+        {
+            if(arr[mid] == 0)
+            {
+                swap(arr,low,mid);
+                low++;
+                mid++;
+            }
+            else if(arr[mid] == 1)
+            {
+                mid++;
+            }
+            else if(arr[mid] == 2)
+            {
+              swap(arr,mid,high);
+              high--;
+            }
+        }
+        
+    }
+}
+ */
+/*
  * Given an array nums with n objects colored red, white, or blue, sort them in-place so that objects of the same color are adjacent, with the colors in the order red, white, and blue.
 
 We will use the integers 0, 1, and 2 to represent the color red, white, and blue, respectively.
