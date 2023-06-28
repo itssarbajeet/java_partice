bdfcc98e676b855ffddd5b7614c1cd4026fe6623
itssarbajjet <sarbajeetmohanty110@gmail.com>
2023-06-28 12:48:52 +0530

    intial

---
+++ Array/EASY/A47_RearrangeArrayInAlternatinPositiveAndNegativeItems.java
@@ -0,0 +1,15 @@
+class A47_RearrangeArrayInAlternatinPositiveAndNegativeItems{
+    public static void rearrange(int arr[]){
+        int index=0;
+        int temp=0;
+        for (int i = 0 ;i<arr.length;i++){
+            while(index!=)
+
+    }
+    public static void main(String args[]){
+        int[]arr={1,2,3,4,-5,-6,-7,-8};
+        rearrange(arr);
+
+    }
+}
+}
