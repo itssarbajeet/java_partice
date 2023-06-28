5bb8b4ea7ce64345b29ca51b8e27bb9a5da482a4
itssarbajjet <sarbajeetmohanty110@gmail.com>
2023-06-28 15:31:09 +0530

    intial

--- Array/EASY/A47_RearrangeArrayInAlternatinPositiveAndNegativeItems.java
+++ Array/EASY/A47_RearrangeArrayInAlternatinPositiveAndNegativeItems.java
@@ -1,15 +1,24 @@
-class A47_RearrangeArrayInAlternatinPositiveAndNegativeItems{
-    public static void rearrange(int arr[]){
-        int index=0;
-        int temp=0;
-        for (int i = 0 ;i<arr.length;i++){
-            while(index!=)
-
+ import java.util.*;
+public class A47_RearrangeArrayInAlternatinPositiveAndNegativeItems{
+    public static String compression(String str){
+        StringBuilder newStr=new StringBuilder("");
+        for(int i=0;i<str.length();i++){
+            Integer count=1;
+            while(i<str.length()-1 && str.charAt(i)==str.charAt(i+1)){
+                count++;
+                i++;
+            }
+            newStr=newStr.append(str.charAt(i));
+            if(count>1){
+                newStr=newStr.append(count.toString());
+            }
+        }
+        return newStr.toString();
     }
-    public static void main(String args[]){
-        int[]arr={1,2,3,4,-5,-6,-7,-8};
-        rearrange(arr);
-
+    public static void main(String[] args) {
+        Scanner sc =new Scanner(System.in);
+        System.out.println("Enter str: ");
+        String str=sc.nextLine();
+        System.out.println(compression(str));
     }
-}
 }
