9fc05034c0f0d8040bb620922eddbd208360e7bc
itssarbajjet <sarbajeetmohanty110@gmail.com>
2023-06-30 15:28:14 +0530

    intial

--- Array/EASY/A47_RearrangeArrayInAlternatinPositiveAndNegativeItems.java
+++ Array/EASY/A47_RearrangeArrayInAlternatinPositiveAndNegativeItems.java
@@ -1,117 +1,74 @@
 import java.util.*;
 public class A47_RearrangeArrayInAlternatinPositiveAndNegativeItems{
-    public static void bubble_sort(int arr[]){
+    public static int[]reArrange(int arr[]){
         int n=arr.length;
-        for(int i=0;i<n-1;i++){
-            for(int j=0;j<n-i-1;j++){
-                if(arr[j]>arr[j+1]){
-                    int temp=arr[j];
-                    arr[j]=arr[j+1];
-                    arr[j+1]=temp;
+        int temp=0;
+        int temp1=0;
+        for (int i = 0;i<n;++i){
+            int check=arr[i];
+            if(i%2==0){
+                if(check<0){
+                    continue;
+                }
+                else{
+                    temp=check;
+                    int index=i+1;
+                while(index<n){
+                    if(arr[index]<0){
+                        temp1=arr[index];
+                        while(index>i){
+                            arr[index]=arr[index-1];
+                            index--;
+                        }
+                        arr[i]=temp1;
+                        arr[i+1]=temp;
+
+                        break;
+                    }  
+                    
+                    index++;
                 }
             }
         }
-        System.out.print("The sorted Array is: ");
-        for(int i=0;i<arr.length;i++){
-        System.out.print(" "+arr[i]);
-        }
-        System.out.println();
-    }
+            else{
+                if(check>0){
+                    continue;
+                }
+                else{
+                    temp=check;
+                    int index=i+1;
+                while(index<n){
+                    if(arr[index]>0){
+                        temp1=arr[index];
+                        while(index>i){
+                            arr[index]=arr[index-1];
+                            index--;
+                        }
+                        arr[i]=temp1;
+                        arr[i+1]=temp;
 
-    public static void selection_sort(int arr[]){
-        int n=arr.length;
-        for(int i=0;i<n-1;i++){
-            int minpos=i;
-            for(int j=i+1;j<n;j++){
-                if(arr[minpos]>arr[j]){
-                    minpos=j;
+                        break;
+                    }   
+                    index++;
                 }
             }
-            int temp=arr[minpos];
-            arr[minpos]=arr[i];
-            arr[i]=temp;
-        }
-        System.out.print("The sorted Array is: ");
-        for(int i=0;i<n;i++){
-        System.out.print(" "+arr[i]);
-        }
-        System.out.println();
-    }
-
-    public static void insertion_sort(int arr[]){
-        int n=arr.length;
-        for(int i=1;i<n;i++){
-            int curr=arr[i];
-            int prev=i-1;
-            while(prev>=0 && arr[prev]>curr){
-                arr[prev+1]=arr[prev];
-                prev--;
             }
-            arr[prev+1]=curr;
-        }
-        System.out.print("The sorted Array is: ");
-        for(int i=0;i<n;i++){
-        System.out.print(" "+arr[i]);
-        }
-        System.out.println();
-    }
-    
-    public static void counting_sort(int arr[]){
-        int largest=Integer.MIN_VALUE;
-        int n=arr.length;
-        for(int i=0;i<n;i++){
-            largest=Math.max(largest,arr[i]);
-        }
-        int count[]=new int[largest+1];
-        for(int i=0;i<n;i++){
-            count[arr[i]]++;
-        }
-        int j=0;
-        for(int i=0;i<n;i++){
-            while(count[i]>0){
-                arr[j]=i;
-                j++;
-                count[i]--;
             }
+            return arr;
+        
         }
-        System.out.print("The sorted Array is: ");
-        for(int i=0;i<n;i++){
-        System.out.print(" "+arr[i]);
-        }
-        System.out.println();
-    }
-
+        
     
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        System.out.println("Enter n: ");
-        int n=sc.nextInt();
-        int arr[]=new int[n];
-        System.out.print("Enter Elements: ");
-        for(int i=0;i<n;i++){
-            arr[i]=sc.nextInt();
+    public static void main(String args[]) {
+        int arr[] = {-5, -2, 5, 2, 4, 7, 1, 8, 0, -8 };
+        reArrange(arr);
+        for(int i: arr){
+            System.out.println(i);
         }
-        System.out.println();
-        System.out.print("Enter The Choice: ");
-        String choice=sc.next();
-        switch(choice){
-            case "bubblesort":
-            bubble_sort(arr);
-            break;
-            case "selectionsort":
-            selection_sort(arr);
-            break;
-            case "insertion_sort":
-            insertion_sort(arr);
-            break;
-            case "countingsort":
-            counting_sort(arr);
-            break;
-            default:
-            System.out.println("Invalid Choice!!!");
-        }
+
     }
 }
+
 
 
 
