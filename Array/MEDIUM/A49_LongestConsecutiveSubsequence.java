5b70a5d7320c6cbb1c27c8fbecb8fc92bf3d4e96
itssarbajjet <sarbajeetmohanty110@gmail.com>
2023-06-30 13:01:17 +0530

    initial

---
+++ Array/MEDIUM/A49_LongestConsecutiveSubsequence.java
@@ -0,0 +1,8 @@
+public class A49_LongestConsecutiveSubsequence {
+    int arr[]={42 ,37, 35 ,14 ,17,10,50 ,20, 44, 13, 4, 33, 12, 28 ,7 ,36 ,46 ,22 ,26, 21, 47, 18 ,31, 2, 5 ,34 ,9 ,45 ,16 ,32, 48 ,19 ,11 ,29, 27 ,39, 8, 49 ,43};
+    Arrays.sort(arr);
+    for(int i : arr){
+        System.out.print(i+" ");
+    }
+
+}
