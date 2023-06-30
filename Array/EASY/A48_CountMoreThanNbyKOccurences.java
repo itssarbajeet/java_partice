db77d2ef0036c5abf6451cd188d32e26be877326
itssarbajjet <sarbajeetmohanty110@gmail.com>
2023-06-30 13:12:45 +0530

    inital

--- Array/EASY/A48_CountMoreThanNbyKOccurences.java
+++ Array/EASY/A48_CountMoreThanNbyKOccurences.java
@@ -1,5 +1,4 @@
 public class A48_CountMoreThanNbyKOccurences {
-    //Function to find all elements in array that appear more than n/k times.
     public static int countOccurence(int[] arr, int n, int k) 
     {
         int count=0;
@@ -20,3 +19,30 @@ public class A48_CountMoreThanNbyKOccurences {
         System.out.println(countOccurence(arr,arr.length,k));
     }
 }
+/*
+ * Given an array of positive integers. Find the length of the longest sub-sequence such that elements in the subsequence are consecutive integers, the consecutive numbers can be in any order.
+ 
+
+Example 1:
+
+Input:
+N = 7
+a[] = {2,6,1,9,4,5,3}
+Output:
+6
+Explanation:
+The consecutive numbers here
+are 1, 2, 3, 4, 5, 6. These 6 
+numbers form the longest consecutive
+subsquence.
+Example 2:
+
+Input:
+N = 7
+a[] = {1,9,3,10,4,20,2}
+Output:
+4
+Explanation:
+1, 2, 3, 4 is the longest
+consecutive subsequence.
+ */
