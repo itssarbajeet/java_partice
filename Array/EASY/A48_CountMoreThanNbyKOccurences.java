8a3e4d6ae05c2d15c134c2d258409903fb8c94e9
itssarbajjet <sarbajeetmohanty110@gmail.com>
2023-06-30 14:55:31 +0530

    inital

--- Array/EASY/A48_CountMoreThanNbyKOccurences.java
+++ Array/EASY/A48_CountMoreThanNbyKOccurences.java
@@ -20,29 +20,17 @@ public class A48_CountMoreThanNbyKOccurences {
     }
 }
 /*
- * Given an array of positive integers. Find the length of the longest sub-sequence such that elements in the subsequence are consecutive integers, the consecutive numbers can be in any order.
- 
+ * Given an array of size n and an integer k, find all elements in the array that appear more than n/k times. 
 
-Example 1:
+Examples:
 
-Input:
-N = 7
-a[] = {2,6,1,9,4,5,3}
-Output:
-6
-Explanation:
-The consecutive numbers here
-are 1, 2, 3, 4, 5, 6. These 6 
-numbers form the longest consecutive
-subsquence.
-Example 2:
+Input: arr[] = {3, 1, 2, 2, 1, 2, 3, 3}, k = 4
+Output: {2, 3}
+Explanation: Here n/k is 8/4 = 2, therefore 2 appears 3 times in the array that is greater than 2 and 3 appears 3 times in the array that is greater than 2
 
-Input:
-N = 7
-a[] = {1,9,3,10,4,20,2}
-Output:
-4
-Explanation:
-1, 2, 3, 4 is the longest
-consecutive subsequence.
+Input: arr[] = {9, 8, 7, 9, 2, 9, 7}, k = 3
+Output: {9}
+Explanation: Here n/k is 7/3 = 2, therefore 9 appears 3 times in the array that is greater than 2.
+
+
  */
--- Array/MEDIUM/A49_LongestConsecutiveSubsequence.java
+++ Array/MEDIUM/A49_LongestConsecutiveSubsequence.java
@@ -29,3 +29,32 @@ public class A49_LongestConsecutiveSubsequence {
     }  
 
 }
+/*
+ * /*
+ * Given an array of positive integers. Find the length of the longest sub-sequence such that elements in the subsequence are consecutive integers, the consecutive numbers can be in any order.
+ 
+
+Example 1:
+
+Input:
+N = 7
+a[] = {2,6,1,9,4,5,3}
+Output:
+6
+Explanation:
+The consecutive numbers here
+are 1, 2, 3, 4, 5, 6. These 6 
+numbers form the longest consecutive
+subsquence.
+Example 2:
+
+Input:
+N = 7
+a[] = {1,9,3,10,4,20,2}
+Output:
+4
+Explanation:
+1, 2, 3, 4 is the longest
+consecutive subsequence.
+ */
+ 
