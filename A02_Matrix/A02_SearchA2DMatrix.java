a1419a98ab904df815b2a2317a087e002c51254d
itssarbajjet <sarbajeetmohanty110@gmail.com>
2023-07-02 16:13:22 +0530

    initial

---
+++ A02_Matrix/A02_SearchA2DMatrix.java
@@ -0,0 +1,28 @@
+    class A02_SearchA2DMatrix {
+    public static boolean searchMatrix(int[][] matrix, int target) {
+        for (int i = 0; i < matrix.length; i++) {
+            for (int j = 0; j < matrix[0].length; j++) {
+                if (matrix[i][j] == target) {
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
+    public static void main(String[] args) {
+           int matrix[][] = {{1, 2, 3, 4},
+                             {5, 6, 7, 8},
+                             {9, 10, 11, 12},
+                             {13, 14, 15,16}};
+            if(searchMatrix(matrix,10)){
+                System.out.println("Element found");
+            }
+            else{
+                 System.out.println("Element not found");
+               
+            }
+
+    }
+}
+
+
