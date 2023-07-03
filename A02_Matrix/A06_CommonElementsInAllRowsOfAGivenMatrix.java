5f8be2e843472163d85560ad6d16b683c59a5eca
itssarbajjet <sarbajeetmohanty110@gmail.com>
2023-07-03 11:59:25 +0530

    intial

---
+++ A02_Matrix/A06_CommonElementsInAllRowsOfAGivenMatrix.java
@@ -0,0 +1,11 @@
+public class A06_CommonElementsInAllRowsOfAGivenMatrix {
+    public static void main(String[] args) {
+        int mat[4][5] = {{1, 2, 1, 4, 8},
+                        {3, 7, 8, 5, 1},
+                        {8, 7, 7, 3, 1},
+                        {8, 1, 2, 7, 9}};
+        
+        
+
+    }
+}
