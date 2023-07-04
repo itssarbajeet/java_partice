d25e0543d2e71db68d36f5901e43021198f25e91
itssarbajjet <sarbajeetmohanty110@gmail.com>
2023-07-05 01:09:47 +0530

    initial

---
+++ GFG/A01_NumberOf1Bits.java
@@ -0,0 +1,18 @@
+package GFG;
+
+public class A01_NumberOf1Bits {
+    static int setBits(int N) {
+        int count = 0;
+        while (N != 0) {
+            if ((N & 1) == 1) {
+                count++;
+            }
+            N = N >> 1;
+        }
+        return count;
+    }
+    public static void main(String[] args) {
+        System.out.println("Number of bits in a number: " +setBits(2));
+    }
+    
+}
