733396f098a3ebf448ced4ba62dee238a59c5444
itssarbajjet <sarbajeetmohanty110@gmail.com>
2023-07-04 16:58:10 +0530

    intial

---
+++ A03_String/A01_ReverseString.java
@@ -0,0 +1,16 @@
+import java.util.*;
+class A01_ReverseString {
+    public static void reverseString(char[] s) {
+        for(int i=0;i<s.length/2;i++){
+            char temp=s[s.length-1-i];
+             s[s.length-1-i]=s[i];
+            s[i]=temp;
+        }
+    }
+    
+    public static void main(String[] args) {
+        char[] s = {'h','e','l','l','o'};
+        reverseString(s);
+        System.out.println("Reversed String: "+Arrays.toString(s)); // Output
+    }
+}
