9838085625a25ed402c23c478775602f57d0febf
itssarbajjet <sarbajeetmohanty110@gmail.com>
2023-07-08 19:04:44 +0530

    intial

---
+++ A04_SearchingAndSorting/A04_CountSquare.java
@@ -0,0 +1,24 @@
+package A04_SearchingAndSorting;
+
+public class A04_CountSquare {
+    class Solution {
+        static int countSquares(int N) {
+            int count = 0;
+            int sqrt = (int) Math.sqrt(N);
+            
+            if (sqrt * sqrt == N) {
+                sqrt--;
+            }
+            
+            for (int i = 1; i <= sqrt; i++) {
+                count++;
+            }
+            
+            return count;
+        }
+    
+    public static void main(String[] args) {
+        System.out.println("Number of squares in a given range: " + countSquares(15));
+    }
+}
+}
