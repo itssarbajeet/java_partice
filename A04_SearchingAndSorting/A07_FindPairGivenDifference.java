701c272c5af603f432223c77060307aa17e5ae6f
itssarbajjet <sarbajeetmohanty110@gmail.com>
2023-07-13 23:32:13 +0530

    inital

--- A04_SearchingAndSorting/A07_FindPairGivenDifference.java
+++ A04_SearchingAndSorting/A07_FindPairGivenDifference.java
@@ -14,7 +14,7 @@ public class A07_FindPairGivenDifference {
             
             if (diff == N) {
                 return true;
-            } else if (diff < N) { 
+            } else if (diff < N) {  
                 right++;
             } else {
                 left++;
