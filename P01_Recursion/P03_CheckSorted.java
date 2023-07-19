8bb258e340901cd68b44ec4d1c43dde296c43fb5
itssarbajjet <sarbajeetmohanty110@gmail.com>
2023-07-19 21:10:32 +0530

    initial

---
+++ P01_Recursion/P03_CheckSorted.java
@@ -0,0 +1,18 @@
+package P01_Recursion;
+
+import java.util.*;
+public class P03_CheckSorted {
+    public static Boolean CheckSort(int []arr,int index){
+        if(arr.length-1==index){
+            return true;
+    }
+    if(!(arr[index]<arr[index+1])){
+        return false;
+    }
+    return CheckSort(arr, ++index);
+    }
+    public static void main(String[] args) {
+        int arr[]={1,2,3,4,5,7,8};
+        System.out.println(CheckSort(arr,0));
+    }
+} 
