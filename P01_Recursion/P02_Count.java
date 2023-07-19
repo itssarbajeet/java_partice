699a9cb24c3d79935a29faebb1ff86635b2a2bb5
itssarbajjet <sarbajeetmohanty110@gmail.com>
2023-07-19 21:09:31 +0530

    initial

---
+++ P01_Recursion/P02_Count.java
@@ -0,0 +1,23 @@
+package P01_Recursion;
+
+public class P02_Count {
+    public static int count(int n,int count, int digit){
+        
+        if(digit==0){
+            return count;
+        }
+        if(n==0){
+            return digit;
+        } 
+        if(n%10==0){
+            return (count(n/10,++count,--digit));
+        }
+        else{
+            return count(n/10,count,--digit);
+        }
+    }
+    public static void main(String args[]){
+        int s=10;
+        System.out.println(count(s,0,((int)Math.log10(s)+1)));
+    }
+}
