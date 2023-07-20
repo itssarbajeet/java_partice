1c11d34bd277a73a2eba546056533a474aa5a78b
itssarbajjet <sarbajeetmohanty110@gmail.com>
2023-07-20 22:05:46 +0530

    initial

--- P01_Recursion/P01_ReverseANumber.java
+++ P01_Recursion/P01_ReverseANumber.java
@@ -1,14 +1,15 @@
-package P01_Recursion;
 
 public class P01_ReverseANumber {
+    static int arr=0;
     public static int reverse(int n,int digit){
         if(n%10==n){
+        
             return n;
         }
         return n%10*(int)Math.pow(10,digit-1)+reverse(n/10, --digit) ;
 
     }
-    public static void main(String args[]){
+     public static void main(String args[]){
 
         System.out.println(reverse(1234,((int)Math.log10(1234)+1)));
     }
